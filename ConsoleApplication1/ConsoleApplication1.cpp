2c2eec43f02f41ff783d3d14dfe4fa30fff838cf
Roman <kerzok8@gmail.com>
2026-07-12 17:04:58 +0300

    added welcome message

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,14 +7,14 @@ int main()
     int num;
     int num1;
     char oper;
-
+    std::cout << "welcome in calculator \n";
     std::cout << "print first number \n";
     std::cin >> num;
     std::cout << "print second number \n";
     std::cin >> num1;
     std::cout << "print  operation (+,-,/,*) \n";
     std::cin >> oper;
-
+    
     if (oper == '+')
         std::cout << num + num1;
 
