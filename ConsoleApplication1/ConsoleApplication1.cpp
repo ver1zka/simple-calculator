59b3a88a8e2ec3b178954e36e447d0c8a43271ff
Roman <kerzok8@gmail.com>
2026-07-12 16:40:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int num;
+    int num1;
+    char oper;
+
+
+    std::cin >> num;
+    std::cin >> num1;
+    std::cin >> oper;
+
+    if (oper == '+')
+        std::cout << num + num1;
+
+    else if (oper == '-')
+        std::cout << num - num1;
+
+    else if (oper == '*')
+        std::cout << num * num1;
+
+    else if (oper == '/')
+        std::cout << num / num1;
+
+
+}
+
+
