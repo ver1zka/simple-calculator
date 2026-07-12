5dd92bc6ed7ce9119f8c2273a352f722919a89b3
Roman <kerzok8@gmail.com>
2026-07-12 17:22:42 +0300

    upgrade while

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@ int main()
     int num1;
     int i = 0;
     char oper;
+    while (i != 1){
     std::cout << "welcome in calculator \n";
     std::cout << "print first number \n";
     std::cin >> num;
@@ -15,7 +16,7 @@ int main()
     std::cin >> num1;
     std::cout << "print  operation (+,-,/,*) \n";
     std::cin >> oper;
-    while (i == 0) {
+    
         if (oper == '+')
             std::cout << num + num1;
 
